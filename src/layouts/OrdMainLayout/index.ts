d41b4b029b0277fa96ab121dd70b963511c1e0b4
AlexisEche <alexis.eche58@gmail.com>
2021-11-20 16:49:54 -0500

    add main layout

--- src/layouts/OrdMainLayout/index.ts
+++ src/layouts/OrdMainLayout/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./order.vue";
+export { default } from "./main.vue";
--- src/layouts/OrdMainLayout/main.vue
+++ src/layouts/OrdMainLayout/main.vue
@@ -1,8 +1,16 @@
 <template>
-	<div class="about">
-		<h1>Holo</h1>
+	<div class="main">
+		<router-view />
 	</div>
 </template>
 <style lang="scss" scoped>
 @import url("./main.scss");
 </style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "OrdMainLayout",
+});
+</script>
