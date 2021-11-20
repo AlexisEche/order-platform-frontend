b6eebc7cfb9f320c4111785a6d8b90bc534097e7
AlexisEche <alexis.eche58@gmail.com>
2021-11-20 17:05:43 -0500

    add children routes to main layout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,14 +3,15 @@ import { RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
 	{
 		path: "/",
-		name: "Home",
-		component: () => import("../views/Home.vue"),
-	},
-	{
-		path: "/about",
-		name: "About",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/About.vue"),
+		name: "Main",
+		component: () => import("@/layouts/OrdMainLayout"),
+		children: [
+			{
+				path: "/",
+				name: "Home",
+				component: () => import("@/pages/OrdHome"),
+			},
+		],
 	},
 ];
 
