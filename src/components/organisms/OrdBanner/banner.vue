188295bb0a21d38fcfb0401c9f06608263c3608a
AlexisEche <alexis.eche58@gmail.com>
2021-11-20 20:09:26 -0500

    add banner component to home page

---
+++ src/components/organisms/OrdBanner/banner.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="banner">
+		<img class="banner--image" :src="healthly" alt="logo" />
+		<div class="banner--description">
+			<p class="banner--description__title">Disfruta saludablemente</p>
+			<p class="banner--description__subtitle">
+				Lo mejor de nuestra gastronomía
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import healthly from "@/assets/healthly.png";
+
+export default defineComponent({
+	name: "OrdBanner",
+	setup() {
+		return {
+			healthly,
+		};
+	},
+});
+</script>
+
+<style lang="scss">
+@import "./banner.scss";
+</style>
