a109b650d7e9fc35fd31bf926dd3622fa39c0769
AlexisEche <alexis.eche58@gmail.com>
2021-11-22 17:42:46 -0500

    update styles and content to card component

--- src/components/organisms/OrdCard/card.vue
+++ src/components/organisms/OrdCard/card.vue
@@ -1,22 +1,47 @@
 <template>
 	<div class="card">
-		<img class="card--image" :src="food.image" alt="" />
+		<img
+			id="show-modal"
+			@click="showModal = true"
+			class="card--image"
+			:src="food.image"
+			alt=""
+		/>
 		<p class="card--name">{{ food.name }}</p>
 		<p class="card--description">{{ food.description }}</p>
 		<div class="card--buy">
 			<p class="card--price">$/. {{ food.price }}</p>
-			<button class="card--add">+</button>
+			<button class="card--add" @click="addPurchase(food)">+</button>
 		</div>
+
+		<transition name="modal">
+			<ord-modal v-if="showModal" @close="showModal = false">
+				<img
+					id="show-modal"
+					@click="showModal = true"
+					class="card--image_modal"
+					:src="food.image"
+					alt=""
+				/>
+				<p class="card--name">{{ food.name }}</p>
+				<p class="card--description">{{ food.description }}</p>
+			</ord-modal>
+		</transition>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import logo from "@/assets/manzana-roja.svg";
 import { Food } from "@/typing/interface";
+import OrdModal from "@/components/organisms/OrdModal";
+import { useStore } from "vuex";
 
 export default defineComponent({
 	name: "OrdCard",
+	components: {
+		OrdModal,
+	},
 	props: {
 		food: {
 			type: Object as () => Food,
@@ -24,8 +49,16 @@ export default defineComponent({
 		},
 	},
 	setup() {
+		const showModal = ref(false);
+		const store = useStore();
+		const addPurchase = (food: Food) => {
+			store.dispatch("addPurchase", food);
+		};
+
 		return {
 			logo,
+			showModal,
+			addPurchase,
 		};
 	},
 });
