6a39603592069b9c9f7d0f5da4b608ea35e3f718
AlexisEche <alexis.eche58@gmail.com>
2021-11-21 20:43:45 -0500

    add purchase component

---
+++ src/components/molecules/OrdPurchase/purchase.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="purchase">
+		<img class="purchase--image" :src="purchase.image" alt="" />
+		<p class="purchase--title">{{ purchase.title }}</p>
+		<p class="purchase--amount">{{ purchase.amount }}</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import healthly from "@/assets/healthly.png";
+
+interface Purchase {
+	image: string;
+	title: string;
+	amount: number;
+}
+
+export default defineComponent({
+	name: "OrdPurchase",
+	props: {
+		purchase: {
+			type: Object as () => Purchase,
+			required: false,
+		},
+	},
+	setup() {
+		return {
+			healthly,
+		};
+	},
+});
+</script>
+
+<style lang="scss">
+@import "./purchase.scss";
+</style>
